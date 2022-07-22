f35ff1cfc00830dcf4ebb338548b72d4092e14cb
ABustos2606 <info@panaceaediciones.com.ar>
2022-07-22 02:55:38 -0300

    exercises 05, 06 & 07 solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    var total = 0;
 
+    if (start < 0 || end < 0 || typeof start != "number" || typeof end != "number"){
+        return "ERROR"
+    } else {
+        if (start < end){
+            for (i = start; i < end+1; i++){
+             total = total + i;
+            }
+        } else {
+            for (i = start; i > end-1; i--){
+                total = total + i;
+            }
+        }
+        return total;
+    }
+
+    
+    return total;
 };
 
 // Do not edit below this line
