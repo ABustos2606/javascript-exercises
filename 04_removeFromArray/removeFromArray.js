5dcbabfe6e67dc74b38daa9f8834ddba7f4f0c9f
ABustos2606 <info@panaceaediciones.com.ar>
2022-07-22 01:49:13 -0300

    exercises 03 & 04 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    var list = args[0];
+    var exclude = [];
 
+    for (let x = 1; x < arguments.length; x++){
+        exclude.push(arguments[x]);
+    }
+
+    for (let i = 0; i < list.length; i++){
+        for (let j = 0; j < exclude.length; j++){
+            if (list[i]===exclude[j]){
+                list.splice(i,1);
+                i -= 1;
+            }
+        }
+        
+    }
+    return list
 };
 
 // Do not edit below this line
