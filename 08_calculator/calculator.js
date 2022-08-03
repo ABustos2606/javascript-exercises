2a2317711d4ac24edc8433c2c8eab7126e68e58b
ABustos2606 <info@panaceaediciones.com.ar>
2022-08-03 20:07:33 -0300

    excersises 8 & 9 solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = 0;
+  for (let i=0; i<nums.length; i++){
+    total = total + nums[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let total = 1;
+  for (let i=0; i<nums.length; i++){
+    total = total * nums[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let counter = num;
+  let total = 1;
+  while (counter > 0){
+    total = counter * total;
+    counter -= 1;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let comp = text.replace(/[. ,\/#!$%\^&\*;:{}=\-_`~()]/gi,"");
+    let back = "";
+    for (let i = comp.length-1; i >=0; i--) {
+        back = back + comp[i];
+      }
+    return (back.toLowerCase() == comp.toLowerCase())
 };
 
 // Do not edit below this line
