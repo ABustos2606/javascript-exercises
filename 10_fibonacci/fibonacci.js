0b0fafb060c9d276e3262f57ad2b619c30cd5ebc
ABustos2606 <info@panaceaediciones.com.ar>
2022-08-03 21:03:28 -0300

    excersises 10 & 11 solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    var fibo = [];
+    fibo[0] = 1;
+    fibo[1] = 1;
+    if (a>0){
+        for (var i = 2; i < a; i++) {
+        fibo[i] = fibo[i - 2] + fibo[i - 1];
+        }
+        return fibo[a-1];
+    } else {
+        return "OOPS"
+    }
+    
 };
 
 // Do not edit below this line
